2121b8e8562d370638f890816e18f07e71fa26b0
prichith cp <prichith@gmail.com>
2024-05-27 20:56:38 +0530

    backend connection

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const bodyparser = require("body-parser");
+const connectDB = require("./src/connection/connection");
+const cors = require("cors");
+
+// load env variables
+dotenv.config({ path: "config.env" });
+const PORT = process.env.PORT || 8080;
+
+// mongodb connection
+connectDB();
+
+app.use(cors());
+
+// parse request to body-parser
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
+// load route
+app.use("/", require("./src/router/products/products"));
+
+app.listen(PORT, () =>
+  console.log(`server is running on http://localhost:${PORT}`)
+);
---
+++ backend/src/router/products/products.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const route = express.Router();
+// const contactList = require('../controller/contact');
+
+// route.post('/contactlist',contactList.add);
+// route.put('/contactlist/:id',contactList.update);
+// route.delete('/contactlist/:id',contactList.delete);
+// route.get('/contactlist/:page/:limit/:search',contactList.pagination);
+
+module.exports = route;
